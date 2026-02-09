520601fe5acf8885597949bdaf45973349b2e486
wxl <gopher095@gmail.com>
2026-02-09 09:40:10 +0800

    chore(assets): 添加应用图标并更新嵌入配置
    添加 build/appicon.png 并通过 embed 嵌入到 main.tmpl.go 中。

--- main.tmpl.go
+++ main.tmpl.go
@@ -7,7 +7,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/asse
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
